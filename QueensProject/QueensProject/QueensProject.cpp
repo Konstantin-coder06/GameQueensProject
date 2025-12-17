2c4f4826157e104ce636f42d6109da7f6c30b85b
Konstantin-coder06 <kocevelikibg06@gmail.com>
2025-12-17 16:44:24 +0200

    Creation of first matrix functions
    create/print/delete matrix and function for the menu

---
+++ QueensProject/QueensProject/QueensProject.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+int** createMatrix(int n, int m) {
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[m] {0};
+    }
+    return matrix;
+}
+void printMatrix(int n, int m, int** matrix) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void deleteMatrix(int n, int** matrix) {
+    for (int i = 0; i < n; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+void printHelp() {
+    cout << "Commands:" << endl;
+    cout << "new N M       - start new game with size N x M" << endl;
+    cout << "play x y      - place queen at (x, y)" << endl;
+    cout << "free          - show all valid cells" << endl;
+    cout << "show          - visualize board" << endl;
+    cout << "save file.txt - save current state" << endl;
+    cout << "load file.txt - load state from file" << endl;
+    cout << "turn          - show whose turn it is" << endl;
+    cout << "help          - show this menu" << endl;
+    cout << "exit          - exit program" << endl;
+}
+int main()
+{
+    cout << "Welcome to the game \"Queens\""<<endl<<endl;
+
+    printHelp();
+	int n,m;
+	cin >> n>> m;
+	cout << "Board " << n << "x" << m<<endl;
+    int** matrix = createMatrix(n, m);
+    printMatrix(n, m, matrix);
+    deleteMatrix(n, matrix);
+ 
+}
+
+
